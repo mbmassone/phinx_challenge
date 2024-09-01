63909210a1fb01b6f3e4f3b824adbf2ce12bb93d
Bernardo Massone <mbmassone@fi.uba.ar>
2024-09-01 12:22:23 -0300

    agg enableCors en backen

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,7 @@ import { PokemonModule } from './pokemon/pokemon.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(PokemonModule);
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
